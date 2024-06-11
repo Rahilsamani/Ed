2cfc360ed8d7819911a2b2e655b3d5c8768d108a
Rahilsamani <rahilahmed1720@gmail.com>
2024-06-11 10:08:04 +0530

    add mail otp functionality

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 
 const OTPSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,28 @@ const OTPSchema = new mongoose.Schema({
     default: Date.now,
     expires: 5 * 60,
   },
+});
+
+//a function -> to send emails of otp by using nodemailer which was created in "/utils/mailSender"
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      'Verification Email from Edtech Platform',
+      otp
+    );
+    console.log('Email sent Successfully: ', mailResponse);
+  } catch (error) {
+    console.log('error occured while sending mails: ', error);
+    throw error;
+  }
+}
+
+OTPSchema.pre('save', async function (next) {
+  if (this.isNew) {
+    await sendVerificationEmail(this.email, this.otp);
+  }
+  next();
 });
 
 module.exports = mongoose.model('OTP', OTPSchema);
