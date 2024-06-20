d7a60fdf62980104f95912a3c2a076c16136787b
Rahilsamani <rahilahmed1720@gmail.com>
2024-06-21 00:09:08 +0530

    update profile slice

--- src/slices/profileSlice.js
+++ src/slices/profileSlice.js
@@ -4,6 +4,7 @@ const initialState = {
   user: localStorage.getItem("user")
     ? JSON.parse(localStorage.getItem("user"))
     : null,
+  loading: false,
 };
 
 const profileSlice = createSlice({
@@ -13,8 +14,11 @@ const profileSlice = createSlice({
     setUser(state, value) {
       state.user = value.payload;
     },
+    setLoading(state, value) {
+      state.loading = value.payload;
+    },
   },
 });
 
-export const { setUser } = profileSlice.actions;
+export const { setUser, setLoading } = profileSlice.actions;
 export default profileSlice.reducer;
